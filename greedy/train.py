584eeac209a3c57459cce39c3f5a5dc080a4cada
Hu-nie <dshshin8227@kyonggi.ac.kr>
2020-11-20 20:49:37 +0900

    donghun

---
+++ greedy/train.py
@@ -0,0 +1,14 @@
+import glob 
+
+def file_path_save(): 
+    filenames = [] 
+    files = sorted(glob.glob("./DETRAC_YOLO_training/*.jpg")) 
+    
+    for i in range(len(files)): 
+        f = open("./train.txt", 'a') 
+        f.write(files[i] + "\n") 
+        
+
+if __name__ == '__main__': 
+    file_path_save()
+
